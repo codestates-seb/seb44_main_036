b64dd82441b4b45a0218701023af7c5b6377bf9f
hyeincha <runprogrmm@gmail.com>
2023-07-06 13:13:33 +0900

    feat: 진행중/종료 상태 관리 로직 수정

--- client/src/components/project/ProjectHeader.tsx
+++ client/src/components/project/ProjectHeader.tsx
@@ -6,13 +6,17 @@ function ProjectHeader() {
   const orders = Object.entries(ORDERS);
   const progress = Object.entries(PROGRESS);
   const [orderParams, setOrderParams] = useSetParams('order', 'recent');
-  const [, setProgressParams] = useSetParams('progress', 'ongoing');
+  const [progressParams, setProgressParams] = useSetParams('progress', 'ongoing');
 
   return (
     <section className='flex justify-between max-w-[1280px] mx-auto mt-45pxr mb-25pxr'>
       <h2 className='text-2xl font-bold'>프로젝트 리스트</h2>
       <div className='text-sm flex-center gap-20pxr'>
-        <select className='w-80pxr' onChange={(e) => setProgressParams(e.target.value)}>
+        <select
+          className='w-80pxr'
+          value={progressParams}
+          onChange={(e) => setProgressParams(e.target.value)}
+        >
           {progress.map(([key, value]) => (
             <option key={key} value={key}>
               {value}
