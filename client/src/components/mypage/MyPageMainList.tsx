cd7263c79f1b9b2eec01e4670b687fd83ceac82e
kimminki <kimminki02@naver.com>
2023-07-07 21:23:17 +0900

    refactor: 메인 아이템 리스트 수정

--- client/src/components/mypage/MyPageMainList.tsx
+++ client/src/components/mypage/MyPageMainList.tsx
@@ -5,6 +5,7 @@ import { MyPageMainItem } from '.';
 // const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 function MyPageMainList() {
+  // 유저 정보 프롭스로 가져와야함, 프로젝트 정보 받아올것 타입 지정할 것
   // const { data: mainProjects, error: mainProjectsError } = useSWR<IProject[]>(
   //   user
   //     ? user.accountType === 'seller'
