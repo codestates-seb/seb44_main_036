c3b276cfd4308aacef6980edde417e45237341f1
Lee Hye Wook <dlgpdnr123@naver.com>
2023-07-07 16:59:49 +0900

    Feat: auth

---
+++ server/src/main/java/com/codestates/server/auth/userdetails/MemberDetailsService.java
@@ -0,0 +1,65 @@
+package com.codestates.server.auth.userdetails;
+
+import com.codestates.server.auth.utils.CustomAuthorityUtils;
+import com.codestates.server.exception.BusinessLogicException;
+import com.codestates.server.exception.ExceptionCode;
+import com.codestates.server.member.entity.Member;
+import com.codestates.server.member.repository.MemberRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Component
+public class MemberDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+    private final CustomAuthorityUtils authorityUtils;
+
+    public MemberDetailsService(MemberRepository memberRepository, CustomAuthorityUtils authorityUtils){
+        this.memberRepository = memberRepository;
+        this.authorityUtils = authorityUtils;
+    }
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Member> optionalMember = memberRepository.findByEmail(username);
+        Member findMember = optionalMember.orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
+
+        return new MemberDetails(findMember);
+    }
+
+    private final class MemberDetails extends Member implements UserDetails{
+        MemberDetails(Member member){
+            setMemberId(member.getMemberId());
+            setEmail(member.getEmail());
+            setPassword(member.getPassword());
+            setRoles(member.getRoles());
+        }
+
+        public Collection<? extends GrantedAuthority> getAuthorities(){
+            return authorityUtils.createAuthorities(this.getRoles());
+        }
+
+        public String getUsername(){
+            return getEmail();
+        }
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        public boolean isEnabled() {
+            return true;
+        }
+    }
+}
