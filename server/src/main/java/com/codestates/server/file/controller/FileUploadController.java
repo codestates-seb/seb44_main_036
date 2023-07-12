17d55cef7d066641cb507a0fbcb37c77979e80bb
Lee Hye Wook <dlgpdnr123@naver.com>
2023-07-12 14:23:01 +0900

    ix: gradle

---
+++ server/src/main/java/com/codestates/server/file/controller/FileUploadController.java
@@ -0,0 +1,26 @@
+package com.codestates.server.file.controller;
+
+import com.codestates.server.file.service.S3UploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+public class FileUploadController {
+
+    private final S3UploadService s3UploadService;
+
+    @PostMapping("/upload")
+    public ResponseEntity uploadFile(@RequestParam("image")MultipartFile multipartFile) throws IOException{
+        String imageUrl = s3UploadService.upload(multipartFile,"images");
+
+        return new ResponseEntity(imageUrl, HttpStatus.CREATED);
+    }
+}
