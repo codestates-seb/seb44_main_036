6b04202f4d769697f5cf20cbc28688f59293a5b0
Lee Hye Wook <dlgpdnr123@naver.com>
2023-07-18 10:22:53 +0900

    Fix: testFile

--- server/src/test/java/com/codestates/server/ServerApplicationTests.java
+++ server/src/test/java/com/codestates/server/ServerApplicationTests.java
@@ -3,7 +3,7 @@ package com.codestates.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ServerApplicationTests {
 
 	@Test
