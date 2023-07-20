74a6dfcc79721dbb74d05a3593d0b33e59d2def5
eyo25 <qutuzm@naver.com>
2023-07-20 16:32:47 +0900

    feat: 폴더 되돌리기

--- client/src/components/Auth/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as AuthInput } from './AuthInput';
-export { default as LoginForm } from './LoginForm';
-export { default as SignUpForm } from './SignUpForm';
-export { default as SocialButton } from './SocialButton';
-export { default as SocialForm } from './SocialForm';
--- client/src/components/UserModal/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as UserModal } from './UserModal';
-export { default as DaumPostcodeButton } from './DaumPostcodeButton';
