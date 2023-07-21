9802d2878cd9d1b4796d9ce1c0c99683a90e1b8c
eyo25 <qutuzm@naver.com>
2023-07-21 14:20:34 +0900

    feat: 자동 로그인 수정

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -4,25 +4,23 @@ import Toast from './components/toast/toast';
 import { useEffect } from 'react';
 import { useAppDispatch } from './hooks/useReducer';
 import userSlice from './reducer/userSlice';
+import { getUserInfo } from './common/api/authApi';
 
 function Root() {
   const dispatch = useAppDispatch();
+
+  const getUser = async () => {
+    const userInfo = await getUserInfo();
+    dispatch(userSlice.actions.logIn(userInfo));
+  };
 
   useEffect(() => {
     const accessToken = localStorage.getItem('accessToken');
     const memberId = localStorage.getItem('accessToken');
     if (accessToken && memberId) {
-      // const userInfo = await getUserInfo();
-      // dispatch(userSlice.actions.logIn(userInfo));
-      dispatch(
-        userSlice.actions.logIn({
-          nickname: 'eyo25',
-          address: null,
-          userImg: null,
-        })
-      );
+      getUser();
     }
-  }, [dispatch]);
+  }, []);
 
   return (
     <>
