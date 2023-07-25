3b829b29a4bb6708df6de2989ce13bbbfd06bf02
kimminki <kimminki02@naver.com>
2023-07-25 14:47:35 +0900

    refactor: MyPageLikeList 리팩토링

--- client/src/components/mypage/MyPageLikeList.tsx
+++ client/src/components/mypage/MyPageLikeList.tsx
@@ -3,11 +3,6 @@ import type { Projects } from '@/common/types/responseTypes';
 import useSWR, { mutate } from 'swr';
 import { userApi } from '@/common/api/api';
 import { Link } from 'react-router-dom';
-import { useEffect, useState, useCallback } from 'react';
-import { isPastDeadline } from '@/common/utils';
-import { Empty } from '../ui';
-
-type Progress = 'all' | 'ongoing' | 'end';
 
 interface UserModalProps {
   memberId?: string;
@@ -19,32 +14,9 @@ function MyPageLikeList({ memberId }: UserModalProps) {
     userApi.getUserProjects
   );
 
-  const [filteredProjects, setFilteredProjects] = useState(projectList);
-
-  const filterByProgress = useCallback((data: Projects, progress: Progress) => {
-    if (progress === 'all') return data;
-    return data.filter((item) =>
-      progress === 'end' ? isPastDeadline(item.expiredDate) : !isPastDeadline(item.expiredDate)
-    );
-  }, []);
-
-  useEffect(() => {
-    const applyFilters = () => {
-      if (!projectList) return;
-      const progress: Progress = 'all'; // update this based on your needs
-
-      const filteredList = filterByProgress(projectList, progress);
-
-      setFilteredProjects(filteredList);
-    };
-    applyFilters();
-  }, [projectList, filterByProgress]);
-
-  if (!filteredProjects || filteredProjects.length === 0) return <Empty />;
-
   return (
     <section className='grid-auto max-w-[1280px] mx-auto'>
-      {filteredProjects.map((project) => (
+      {projectList?.map((project) => (
         <Link to={`/project/${project.projectId}`} key={project.projectId}>
           <MyPageLikeItem project={project} projects={projectList ?? []} />
         </Link>
