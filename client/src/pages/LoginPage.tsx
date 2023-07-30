e9c86b865b5f5af0d516092ba9ac65c534280d21
eyo25 <qutuzm@naver.com>
2023-07-30 19:51:34 +0900

    feat: 구글 로그인 구현

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -3,7 +3,7 @@ import { LoginForm, SocialForm } from '@/components/auth';
 function LoginPage() {
   return (
     <section className='flex-col flex-center my-140pxr'>
-      {/* <SocialForm /> */}
+      <SocialForm />
       <article className='w-full max-w-[370px] mt-42pxr'>
         <LoginForm />
       </article>
